57e0e426fae3660cd023c805cb42bc912cca782e
An <79509067+andtr-2021@users.noreply.github.com>
2023-12-03 16:51:17 +0700

    Update step1-script.sh

--- step1-script.sh
+++ step1-script.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Set variables
 KEY_NAME="MyKeyPair100"
 SECURITY_GROUP_NAME="MyServer100"
@@ -11,13 +9,10 @@ PORT_TOMCAT=8080
 
 # Create key pair
 aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > "$KEY_NAME.pem"
-
 # Create a security group
 command_output=$(aws ec2 create-security-group --group-name $SECURITY_GROUP_NAME --description "My security group" --vpc-id $VPC_ID)
-
 # Extract GroupId from the command output using jq (assuming JSON format)
 group_id=$(echo "$command_output" | jq -r '.GroupId')
-
 # Print or use the GroupId as needed
 echo "GroupId: $group_id"
 
@@ -29,16 +24,11 @@ authorize_security_group() {
         --port $1 \
         --cidr 0.0.0.0/0
 }
-
 # Authorize SSH port
 authorize_security_group $PORT_SSH
-
 # Authorize Tomcat port
 authorize_security_group $PORT_TOMCAT
-
 # Launch the instance
 aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type t2.micro --key-name $KEY_NAME --security-group-ids $group_id --subnet-id $SUBNET_ID
-
 # Edit permissions for the file owner
 chmod 600 "$KEY_NAME.pem"
-
