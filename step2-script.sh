8b5573043d59ada48afcfbd727be8eb66260bfb6
An <79509067+andtr-2021@users.noreply.github.com>
2023-12-03 22:15:25 +0700

    Update step2-script.sh

--- step2-script.sh
+++ step2-script.sh
@@ -1,3 +1,13 @@
+# RMIT University Vietnam
+# Course: COSC2767 Systems Deployment and Operations
+# Semester: 2023B
+# Assessment: Assignment 1
+# Author: Do Truong An
+# ID: s3878698
+# Created  date: 28/11/2023
+# Last modified: 3/13/2023
+# Acknowledgement:
+
 repo_url="https://github.com/andtr-2021/COSC2767_Assignment1_BashScript_AWS_s3878698.git"
 github_token="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"
 
